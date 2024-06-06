afe2f98a205ae8e310faaa5a1996739baba55212
bbkx226 <bbkx226@gmail.com>
2024-06-06 15:59:35 +0800

    first commit

---
+++ src/main/java/com/mycompany/dcoms_asm/DCOMS_ASM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dcoms_asm;
+
+/**
+ *
+ * @author bbkx2
+ */
+public class DCOMS_ASM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
