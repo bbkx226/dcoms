ede2b6825db3228b305000ba8cc46a38c471134c
qianjiewong <qianjiewong@gmail.com>
2024-06-16 18:39:53 +0800

    Customer order and payment for client

--- src/main/java/client/pages/CustomerInterface.java
+++ src/main/java/client/pages/CustomerInterface.java
@@ -1,10 +1,15 @@
 package client.pages;
 
+import client.CustomerActions;
+import client.FoodActions;
 import client.components.Menu;
 import models.User;
 import utils.InputUtils;
 import utils.UIUtils;
 
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
@@ -12,13 +17,13 @@ import java.util.Scanner;
 public class CustomerInterface {
     private User user;
 
-    public void customerMenu(User user) {
+    public void customerMenu(User user) throws MalformedURLException, NotBoundException, RemoteException {
         this.user = user;
         Scanner scanner = new Scanner(System.in); // Fix typo: System.io should be System.in
 
         while (true) {
             String fullName = user.getFirstName() + " " + user.getLastName();
-            List<String> options = List.of("View Menu", "Order", "Back");
+            List<String> options = List.of("View Menu", "Order", "Checkout", "Back");
             Menu menu = new Menu(" Welcome to McGee, " + fullName + "!!!", options, "Enter your choice:", 60);
             try {
                 UIUtils.clrscr();
@@ -26,13 +31,13 @@ public class CustomerInterface {
 
                 switch (menu.getInput()) {
                     case 1:
-//                        viewMenu(scanner);
+                        FoodActions.viewMenu();
                         break;
                     case 2:
-//                        orderMenu();
+                        new OrderInterface(user).orderMenu(); //Order, Update, Delete Food
                         break;
                     case 3:
-//                        checkOrder();
+                        CustomerActions.checkOrder(user); // Check orders in cart & proceed to payment
                         break;
                     case 4: // Back option
                         return; // Return to the previous menu
